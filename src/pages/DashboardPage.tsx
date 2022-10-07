44aa26837074a902f7cddb4d70cf88ed20e673d5
김민욱 <minuk3508@gmail.com>
2022-10-08 01:32:22 +0900

    chart data 그래프 구현

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,9 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Template } from "Template/Template";
 import styled from "styled-components";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import HumidityGraph from "./HumidityGraph";
+
+interface channelProps {
+  id: number;
+  name: string;
+  description: string;
+  latitude: string;
+  longitude: string;
+  field1: string;
+  field2: string;
+  field3: string;
+  created_at: string;
+  updated_at: string;
+  last_entry_id: number;
+}
+
+interface feedProps {
+  created_at: string;
+  entry_id: number;
+  field1: string;
+  field2: string;
+  field3: string;
+}
+
+interface dataProps {
+  channel: channelProps;
+  feeds: feedProps[];
+}
 
 export function DashboardPage() {
+  const [channelInfo, setChannelInfo] = useState<channelProps>();
+  const [feedData, setFeedData] = useState<feedProps[]>();
   const API_KEY = "6SKW0U97IPV2QQV9";
   const CHAEENEL_ID = "1348864";
   useEffect(() => {
@@ -12,36 +41,16 @@ export function DashboardPage() {
         `https://api.thingspeak.com/channels/${CHAEENEL_ID}/feeds.json?api_key=${API_KEY}`
       );
       const json = await res.json();
+      setChannelInfo(json.channel);
+      setFeedData(json.feeds);
       console.log(json);
     })();
   }, []);
-
-  const onDragEnd = (result: any) => {
-    if (!result.destination) {
-      return;
-    }
-  };
-  const items = [{ id: 0, content: 1 }];
+  console.log(channelInfo);
   return (
     <Template>
       <Container>
-        {/* <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="test">
-            {(provided, snapshot) => (
-              <div ref={provided.innerRef}>
-                {items.map((item) => (
-                  <Draggable key={item.id} draggableId={item.id}>
-                    {(provided, snapshot) => (
-                      <div>
-                        <div ref={provided.innerRef}>{item.content}</div>
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext> */}
+        <HumidityGraph channelInfo={channelInfo} feedData={feedData} />
       </Container>
     </Template>
   );
@@ -50,9 +59,4 @@ const Container = styled.div`
   display: flex;
   width: 100%;
   height: 100%;
-`;
-const TestDiv = styled.div`
-  width: 100px;
-  height: 100px;
-  border: 1px solid black;
 `;
