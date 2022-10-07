cc3b4ba9c11a1f51bb059806ce8960b7529bf16e
hoonjo1 <hoonjo1@gmail.com>
2022-10-07 17:16:25 +0900

    sample table 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,18 @@
-import { Template } from "Template/Template";
+import { Template } from 'Template/Template';
+import useFetch from 'hooks/useFetch';
+
+import Table from 'api/components/Table';
+import Loader from 'api/components/Loader';
 
 export function MainPage() {
-  return <Template>Main Here</Template>;
+  const { results, loading } = useFetch();
+  return (
+    <Template>
+      {loading && results.length === 0 ? (
+        <Loader />
+      ) : (
+        <Table results={results} />
+      )}
+    </Template>
+  );
 }
