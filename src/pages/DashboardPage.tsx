83f7dfa79237e8d343d630d61f8d985c94a76093
ygh0627 <ygh970627@gmail.com>
2022-10-08 15:17:38 +0900

    Add : 그래프 3개 추가

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,47 +1,17 @@
-import { useEffect } from "react";
 import { Template } from "Template/Template";
 import styled from "styled-components";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-
-export function DashboardPage() {
-  const API_KEY = "6SKW0U97IPV2QQV9";
-  const CHAEENEL_ID = "1348864";
-  useEffect(() => {
-    (async () => {
-      const res = await fetch(
-        `https://api.thingspeak.com/channels/${CHAEENEL_ID}/feeds.json?api_key=${API_KEY}`
-      );
-      const json = await res.json();
-      console.log(json);
-    })();
-  }, []);
-
-  const onDragEnd = (result: any) => {
-    if (!result.destination) {
-      return;
-    }
-  };
-  const items = [{ id: 0, content: 1 }];
+import useFetchWeatherData from "hooks/useFetchWeatherData";
+import DegreeGraph from "./DegreeGraph.pages";
+import HumidityGraph from "./HumidityGraph.pages";
+import PressureGraph from "./PressureGraph.pages";
+function DashboardPage() {
+  const weatherData = useFetchWeatherData();
   return (
     <Template>
       <Container>
-        {/* <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="test">
-            {(provided, snapshot) => (
-              <div ref={provided.innerRef}>
-                {items.map((item) => (
-                  <Draggable key={item.id} draggableId={item.id}>
-                    {(provided, snapshot) => (
-                      <div>
-                        <div ref={provided.innerRef}>{item.content}</div>
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext> */}
+        {/* <DegreeGraph /> */}
+        {/* <HumidityGraph /> */}
+        <PressureGraph />
       </Container>
     </Template>
   );
@@ -56,3 +26,5 @@ const TestDiv = styled.div`
   height: 100px;
   border: 1px solid black;
 `;
+
+export default DashboardPage;
---
+++ src/pages/PressureGraph.pages.tsx
@@ -0,0 +1,58 @@
+import Chart from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+import { CategoryScale } from "chart.js";
+import styled from "styled-components";
+import useFetchWeatherData from "hooks/useFetchWeatherData";
+import moment from "moment";
+import { useMemo } from "react";
+Chart.register(CategoryScale);
+
+function PressureGraph() {
+  const weatherData = useFetchWeatherData();
+  const defaultDate = useMemo(() => {
+    return moment(weatherData?.feeds.slice(-1)[0].created_at).format("DD");
+  }, [weatherData]);
+
+  const availableDates = useMemo(() => {
+    const result = weatherData?.feeds.map((feed) =>
+      moment(feed.created_at).format("DD")
+    );
+    return [...new Set(result)];
+  }, [weatherData]);
+
+  const data = {
+    labels: weatherData?.feeds
+      ?.filter(
+        (feed) =>
+          moment(feed.created_at.slice(0, -1)).format("DD") === defaultDate
+      )
+      .map((i) =>
+        moment(i.created_at.slice(0, i.created_at.length - 1)).format("HH:mm")
+      ),
+    datasets: [
+      {
+        label: "기압 그래프",
+        data: weatherData?.feeds
+          ?.filter(
+            (feed) => moment(feed.created_at).format("DD") === defaultDate
+          )
+          .map((i) => i.field2),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+  return (
+    <Div>
+      <h1>Chart Test</h1>
+      <Line data={data} />
+    </Div>
+  );
+}
+const Div = styled.div`
+  width: 80rem;
+  height: 25rem;
+`;
+
+export default PressureGraph;
