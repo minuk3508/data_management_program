ca0f0974b3d82d314cb85fa9b32b26022e6e08b5
minjae46 <minjaejo46@gmail.com>
2022-10-09 19:55:07 +0900

    대쉬보드 이동 버튼 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,23 @@
-import { Template } from 'Template/Template';
-import useFetch from 'hooks/useFetch';
-
-import Loader from 'components/Loader';
-import Table from 'components/Table';
+import { Template } from "Template/Template";
+import useFetch from "hooks/useFetch";
+import Loader from "components/Loader";
+import Table from "components/Table";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export function MainPage() {
   const { results, loading } = useFetch();
+  const navigate = useNavigate();
+
+  const goToDashboard = () => {
+    navigate("/dashboard");
+  };
 
   return (
     <Template>
+      <GoDashboardBtn onClick={goToDashboard}>
+        <BtnText>Dashboard</BtnText>
+      </GoDashboardBtn>
       {loading && results.length === 0 ? (
         <Loader />
       ) : (
@@ -17,3 +26,27 @@ export function MainPage() {
     </Template>
   );
 }
+
+const GoDashboardBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+  margin: 20px 0 0 50px;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px grey;
+
+  :hover {
+    cursor: pointer;
+  }
+
+  @media ${({ theme }) => theme.device.tabletL} {
+    margin-left: 15px;
+  }
+`;
+
+const BtnText = styled.span`
+  text-align: center;
+  padding: 5px 0;
+`;
